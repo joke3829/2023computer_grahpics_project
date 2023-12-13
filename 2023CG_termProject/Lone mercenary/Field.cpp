044d9d7b993139461ff119053949b38ff24db4f1
Do-0-K <rlaehdudqkqh@naver.com>
2023-12-13 19:22:18 +0900

    머리 충돌-보너스 대미지, 다른 부위 충돌

--- 2023CG_termProject/Lone mercenary/Field.cpp
+++ 2023CG_termProject/Lone mercenary/Field.cpp
@@ -37,7 +37,7 @@ void Field::Update()
 		if (aliving < max_alive) {
 			if (not enemy_list[i]->Death_check()) {
 				enemy_list[i]->setPlayerLoc(dynamic_cast<Player*>(mPlayer)->getLoc());
-				//enemy_list[i]->walk_ani(0);
+				enemy_list[i]->walk_ani(0);
 				/*if (dynamic_cast<NM_zombie*>(enemy_list[i])->getlarm()->collision_check(*mField->gethouse_1())
 					or dynamic_cast<NM_zombie*>(enemy_list[i])->getrarm()->collision_check(*mField->gethouse_1())
 					or dynamic_cast<NM_zombie*>(enemy_list[i])->getlarm()->collision_check(*mField->gethouse_2())
